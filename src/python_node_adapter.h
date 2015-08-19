cb56de11cae16af8ad25859836e71b27c4d2ea4f
buck <sebastian.buck@uni-tuebingen.de>
2015-08-19 02:31:48 +0200

    restructuring

--- src/python_node_adapter.h
+++ src/python_node_adapter.h
@@ -2,7 +2,7 @@
 #define PYTHON_NODE_ADAPTER_H
 
 /// PROJECT
-#include <csapex/view/default_node_adapter.h>
+#include <csapex/view/node/default_node_adapter.h>
 
 /// COMPONENT
 #include "python_node.h"
